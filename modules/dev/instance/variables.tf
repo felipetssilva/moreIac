5a29c3e353e231b6d14a71e311e3e7824fd75a51
felipetssilva <felipetssilva@hotmail.com>
2024-07-15 22:39:41 +0000

    fix indentation

--- modules/dev/instance/variables.tf
+++ modules/dev/instance/variables.tf
@@ -1,13 +1,11 @@
-1  # --- instance/variables.tf ---
-2  
-3  variable "instance_type" {
-4    type        = string
-5  }
-6  
-7  variable "subnet_id" {
-8    type        = string
-9  }
-10 
-11 variable "instance_count" {
-12   type        = number
-13 }
+variable "instance_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "instance_count" {
+  type = number
+}
--- terraform/dev/us-east-1/t2.micro/ec2-deployment/variables.tf
+++ terraform/dev/us-east-1/t2.micro/ec2-deployment/variables.tf
@@ -1,19 +1,19 @@
 # --- root/variables.tf ---
 
 variable "az" {
-  type        = "string"
+  type        = string
   description = "Availability zone for subnet"
   default     = "us-east-1a"
 }
 
 variable "vpc_cidr_block" {
-  type        = "string"
+  type        = string
   description = "CIDR Block for VPC"
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
-  type        = "string"
+  type        = string
   description = "CIDR Block for Subnet"
   default     = "10.0.0.0/24"
 }

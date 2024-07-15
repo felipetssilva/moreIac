97114578810bab2a9fa77fd3dfbc8373497bdb7a
felipetssilva <felipetssilva@hotmail.com>
2024-07-15 22:36:58 +0000

    add more files and variables

--- terraform/dev/us-east-1/t2.micro/ec2-deployment/main.tf
+++ terraform/dev/us-east-1/t2.micro/ec2-deployment/main.tf
@@ -8,3 +8,18 @@ locals {
   location      = local.cwd[2]
   environment   = local.cwd[3]
 }
+
+module "instance" {
+  source = "../../../../../../modules/dev/instance"
+  instance_type = local.instance_type
+  instance_count = 2
+  subnet_id = module.vpc.subnet_id
+}
+
+module "vpc" {
+  source = "../../../../../../modules/dev/vpc"
+  az = var.az
+  vpc_cidr_block = var.vpc_cidr_block
+  subnet_cidr_block = var.subnet_cidr_block
+}
+
